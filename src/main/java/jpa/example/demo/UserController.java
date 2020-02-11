bc1c75f041ff4e225678770613bad773533d49dc
Baodong <wwwicbd@gmail.com>
2020-02-11 10:48:11 +0800

    default_order_pagination

--- src/main/java/jpa/example/demo/UserController.java
+++ src/main/java/jpa/example/demo/UserController.java
@@ -1,6 +1,8 @@
 package jpa.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +19,27 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    // TODO: index method
+    /**
+     * Hibernate: select user0_.id as id1_0_, user0_.email as email2_0_, user0_.name as name3_0_ from user user0_ limit ?, ?
+     * Hibernate: select count(user0_.id) as col_0_0_ from user user0_
+     *
+     * @param page
+     * @param size
+     * @return
+     */
+    @GetMapping
+    public Page<User> index(@RequestParam(required = false) Integer page, @RequestParam(required = false) Integer size) {
+        if (size == null) {
+            size = 20;
+        }
+        if (page == null) {
+            page = 1;
+        }
+        page = (page >= 1) ? page : 1;
+
+        PageRequest pageRequest = PageRequest.of(page - 1, size);
+        return userRepository.findAll(pageRequest);
+    }
 
     @GetMapping("/{id}")
     public Optional<User> show(@PathVariable Long id) {
